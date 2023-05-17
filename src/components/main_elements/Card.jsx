876ea88bc1c65a048bb9a622bf970942ba3db3e1
Abdulmecid Demirhan <abdulmecit.demirhan1@gmail.com>
2023-05-17 15:57:03 +0300

    Designs of currencies and currency cards finished

--- src/components/main_elements/Card.jsx
+++ src/components/main_elements/Card.jsx
@@ -1,7 +1,7 @@
 const Card = (props) => {
   return (
     <>
-      <div className="h-auto w-auto bg-[#ffff] font-main rounded-md">
+      <div className="h-auto w-auto sm:flex-col md-flex-row xl:flex-row bg-[#ffff] font-main rounded-md">
         {props.children}
       </div>
     </>
