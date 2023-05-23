8ca503ab7484b331e12f48757d0862ca82a22225
Abdulmecid Demirhan <abdulmecit.demirhan1@gmail.com>
2023-05-23 17:15:27 +0300

    Added country props which using with Context api to general state and added loading animation

--- src/components/main_elements/Dropdown.jsx
+++ src/components/main_elements/Dropdown.jsx
@@ -2,7 +2,7 @@ const Dropdown = (props) => {
   return (
     <>
       <select className="rounded-xl mt-1 text-aquamarine-blue-900 bg-aquamarine-blue-50 h-auto w-auto font-main">
-        <option className=""> {props.children}</option>
+        {props.children}
       </select>
     </>
   );
