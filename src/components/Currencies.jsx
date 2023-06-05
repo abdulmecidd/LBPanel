e87ac99c71b532fe3315004aac7c29b1aecf9f43
Abdulmecid Demirhan <abdulmecit.demirhan1@gmail.com>
2023-06-05 21:14:55 +0300

    Currencies widget was finished.

--- src/components/Currencies.jsx
+++ src/components/Currencies.jsx
@@ -1,22 +1,59 @@
+import { useEffect, useState } from "react";
 import Card from "./main_elements/Card";
 import Currency from "./main_elements/Currencies/Currency";
+import Loading from "./main_elements/LoadingAnimation/Loading";
 
 const Currencies = () => {
+  const [usdPrice, setUsdPrice] = useState("");
+  const [usdChange, setUsdChange] = useState("");
+  const [eurChange, setEurChange] = useState("");
+  const [EurUsdChange, setEurUsdChange] = useState("");
+  const [eurPrice, setEurPrice] = useState("");
+  const [eurUsdPrice, setEurUsdPrice] = useState("");
+  useEffect(() => {
+    const ws = new WebSocket("wss://nc.ciner.com.tr/sub/htfinans");
+
+    ws.onopen = () => {
+      console.log("socket connected");
+    };
+    ws.onmessage = (e) => {
+      const data = JSON.parse(e.data);
+      if (data.SecuritySlug.includes("dolar")) {
+        setUsdPrice(data.Ask);
+        setUsdChange(data.PercentChange.substr(1));
+      } else if (data.SecuritySlug.includes("euro")) {
+        setEurPrice(data.Ask);
+        setEurChange(data.PercentChange.substr(1));
+      } else if (data.SecuritySlug.includes("eur-usd")) {
+        setEurUsdPrice(data.Ask);
+        setEurUsdChange(data.PercentChange.substr(1));
+      }
+    };
+
+    ws.onerror = (err) => {
+      console.log(err);
+    };
+  });
   return (
     <>
       <Card>
         <header className="rounded-t-xl bg-gradient-to-t from-blue to-dodger-blue-600 h-auto p-[1rem]">
           <h1 className="mb-2 font-medium text-4xl text-[white]">Currencies</h1>
-          <input
-            type="number"
-            className="rounded-xl pl-[1rem] font-medium text-gray"
-            placeholder="$1"
-          />
         </header>
-        <section className="p-[2rem]">
-          <Currency currency="EUR" value="0" />
-          <Currency currency="USD" value="0" />
-          <Currency currency="TRY" value="0" />
+        <section className="p-[1rem]">
+          {usdPrice ? (
+            <>
+              <Currency currency="USD" value={usdPrice} change={usdChange} />
+              <Currency currency="EUR" value={eurPrice} change={eurChange} />
+              <Currency
+                currency="EUR/USD"
+                value={eurUsdPrice}
+                change={EurUsdChange}
+              />
+            </>
+          ) : (
+            <Loading />
+          )}
         </section>
       </Card>
     </>
--- src/components/main_elements/Currencies/Currency.jsx
+++ src/components/main_elements/Currencies/Currency.jsx
@@ -1,18 +1,47 @@
+import { useEffect, useState } from "react";
+
 const Currency = (props) => {
+  const [previousValue, setpreviousValue] = useState("");
+  const [previousChange, setPreviousChange] = useState("");
+
+  useEffect(() => {
+    if (previousChange !== props.change) {
+      setPreviousChange(props.change);
+    }
+
+    if (props.value !== previousValue) {
+      setpreviousValue(props.value);
+    }
+  }, [props.value]);
   return (
     <>
       <div className="flex items-center gap-4 mb-4">
-        <div className="ping">
-          <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-dodger-blue-500 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-dodger-blue-500"></span>
-          </span>
-        </div>
         <div className="currency font-semibold ml-2">
           <p>{props.currency}</p>
         </div>
         <div className="value font-extralight ml-auto text-gray">
-          <p>{props.value}</p>
+          <p
+            className={
+              props.value === previousValue
+                ? `font-light text-[black]`
+                : props.value > previousValue
+                ? "font-bold text-[green]"
+                : "font-bold text-[red]"
+            }
+          >
+            {previousValue}
+          </p>
+          <p
+            className={
+              previousChange > props.change || props.change.startsWith("-", 0)
+                ? `text-[red] text-[0.8rem] text-right font-semibold`
+                : previousChange === props.change
+                ? "text-[black] text-right text-[0.8rem] font-semibold"
+                : `text-[green] text-[0.8rem] text-right font-semibold`
+            }
+          >
+            {previousChange}%
+          </p>
         </div>
       </div>
     </>
