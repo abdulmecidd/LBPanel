5e3b109a641af3b8378d8a36570f68cfb519d367
Abdulmecid Demirhan <abdulmecit.demirhan1@gmail.com>
2023-06-14 18:53:32 +0300

    added className props to change design of the card wherever developer wants

--- src/components/main_elements/Card.jsx
+++ src/components/main_elements/Card.jsx
@@ -1,7 +1,9 @@
 const Card = (props) => {
   return (
     <>
-      <div className="h-auto w-auto sm:flex-col md-flex-row xl:flex-row bg-[#ffff] font-main rounded-md">
+      <div
+        className={`h-auto w-auto sm:flex-row md-flex-row xl:flex-row bg-[#ffff] font-main rounded-md  ${props.className}`}
+      >
         {props.children}
       </div>
     </>
