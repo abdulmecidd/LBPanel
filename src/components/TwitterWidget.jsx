e75214f4e7e3bf1ae44239c4e184f0e9bfbeb799
Abdulmecid Demirhan <abdulmecit.demirhan1@gmail.com>
2023-06-14 18:52:17 +0300

    added url to every topics, design changes

--- src/components/TwitterWidget.jsx
+++ src/components/TwitterWidget.jsx
@@ -1,27 +1,64 @@
+import { useEffect, useState } from "react";
 import Card from "./main_elements/Card";
-import Topics from "./main_elements/Twitter/Topics";
 import TrendTopics from "./main_elements/Twitter/TrendTopics";
 import TwitterFeed from "./main_elements/Twitter/TwitterFeed";
+import axios from "axios";
+import { TWITTER_API_KEY } from "../api";
+
 const TwitterWidget = () => {
+  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchTweets = async () => {
+      try {
+        const response = await axios.get(
+          "https://twitter154.p.rapidapi.com/user/tweets",
+          {
+            method: "GET",
+            params: {
+              username: "awscloud",
+              limit: "10",
+              include_replies: "false",
+              include_pinned: "false",
+            },
+            headers: {
+              "X-RapidAPI-Key": TWITTER_API_KEY,
+              "X-RapidAPI-Host": "twitter154.p.rapidapi.com",
+            },
+          }
+        );
+        setData(response.data.results);
+        setName(data[0].user.username);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTweets();
+  }, []);
+
   return (
     <>
-      <Card>
+      <Card className="max-w-[40rem]">
         <header className="bg-gradient-to-t from-dodger-blue-300 to-dodger-blue-400 text-[white] rounded-t-md p-[1rem]">
           <div className="flex gap-1 items-baseline">
             <h1 className="text-4xl font-semibold">#Tweets</h1>
-            <p className="text-[0.8rem]">
-              From{" "}
-              <a className="hover:underline" href="#">
-                @AmazonAWS
-              </a>
-            </p>
           </div>
         </header>
-        <section className="flex">
+        <section className="xl:flex md:flex ">
           <div className="feed w-auto overflow-y-scroll max-h-96">
-            <TwitterFeed />
-            <TwitterFeed />
-            <TwitterFeed />
+            {data.map((e, i) => {
+              return (
+                <TwitterFeed
+                  key={i}
+                  name={e.user.username}
+                  screen_name={e.user.name}
+                  content={e.text}
+                  profileImg={e.user.profile_pic_url}
+                  image_url={e.media_url}
+                />
+              );
+            })}
           </div>
           <div className="topics">
             <TrendTopics />
--- src/components/main_elements/Twitter/TrendTopics.jsx
+++ src/components/main_elements/Twitter/TrendTopics.jsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from "react";
 import Topics from "./Topics";
+import axios from "axios";
+import { TWITTER_API_KEY } from "../../../api";
 
 const TrendTopics = () => {
+  const [trends, setTrends] = useState([]);
+
+  useEffect(() => {
+    fetchTrends();
+  }, []);
+
+  const fetchTrends = async () => {
+    try {
+      const response = await axios.get(
+        "https://twitter154.p.rapidapi.com/trends/",
+        {
+          headers: {
+            "X-RapidAPI-Key": TWITTER_API_KEY,
+            "X-RapidAPI-Host": "twitter154.p.rapidapi.com",
+          },
+          params: { woeid: "2344116" },
+        }
+      );
+      setTrends(response.data[0].trends);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <div className="m-4">
+      <div className="m-4 max-h-96 overflow-y-auto">
         <h1 className="p-[0.5rem] font-semibold">Trend Topics</h1>
 
-        <Topics name="Games" tweetVolume="12312" />
-        <Topics name="#MakeUp" tweetVolume="56515" />
-        <Topics name="#Election" tweetVolume="200" />
-        <Topics name="Minecraft" tweetVolume="12312" />
+        {trends &&
+          trends.map((e, key) => {
+            return (
+              <Topics
+                key={key}
+                name={e.name}
+                tweetVolume={e.tweet_volume ? e.tweet_volume : "-"}
+                url={e.url}
+              />
+            );
+          })}
       </div>
     </>
   );
